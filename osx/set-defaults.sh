03b5c66dfe8e230ae1a0a81255f46393aab9d73b
Rich Gilbank <richgilbank@gmail.com>
2014-12-17 10:46:11 -0500

    Use homebrew ctags

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -6,7 +6,7 @@ killall Finder
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install homebrew stuffs
-brew install git the_silver_searcher
+brew install git the_silver_searcher ctags
 
 # Install fonts
 git clone https://github.com/powerline/fonts.git
